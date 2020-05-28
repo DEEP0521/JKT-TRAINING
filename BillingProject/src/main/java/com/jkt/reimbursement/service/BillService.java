ecc1cec19d928d2aa9e08a6599c5d3b9b4f36f40
Faisal-debug <faisal3000khan@gmail.com>
2020-05-28 23:02:26 +0530

    bill entity commit

--- BillingProject/src/main/java/com/jkt/reimbursement/service/BillService.java
+++ BillingProject/src/main/java/com/jkt/reimbursement/service/BillService.java
@@ -1,5 +1,34 @@
 package com.jkt.reimbursement.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jkt.reimbursement.entity.Bill;
+import com.jkt.reimbursement.repository.BillRepository;
+
+@Service
 public class BillService {
+	
+	@Autowired
+	private BillRepository billRepo;
+	
+	public void postBill(Bill bill)
+	{
+		billRepo.save(bill);
+	}
+	
+	public List<Bill> getBills()
+	{
+		return billRepo.findAll();
+		
+	}
+	
+	public void delBill(int id)
+	{
+		Bill bill=billRepo.getOne(id);
+		billRepo.delete(bill);
+	}
 
 }
